41aa41c573c2af5c99969b91cee17c3895bb8b02
Larissa Ferreira Pardim <85230273+laripardim@users.noreply.github.com>
2022-02-24 11:26:17 -0300

    Add files via upload

---
+++ proj_velaris/login/script.js
@@ -0,0 +1,92 @@
+const email = document.querySelector("#email");
+const senha = document.querySelector("#senha");
+const camera = document.querySelector("#camera");
+const foto = document.querySelector("#foto");
+const login = document.querySelector("#login");
+const cadastro = document.querySelector("#cadastro");
+
+var imagem = "";
+
+camera.addEventListener("Click", () => {
+    foto.click();
+})
+
+foto.addEventListener("change", () => {
+    let file = e.target.files[0];
+
+    let reader = new FileReader();
+
+    reader.onload = (data) => {
+        console.log(data);
+        console.log(data.target.result);
+        imagem = data.target.result;
+    }
+
+    reader.readAsDataURL(file);
+});
+
+function verificaLogin() {
+    if(localStorage.getItem('userdata') != null) {
+        window.location.href = '../home';
+    }
+}
+
+function conectar() {
+    let data = JSON.stringify({
+        email: email.ariaValueMax,
+        senha: md5(senha.value),
+    });
+
+    fetch("http://localhost:3000/login", {
+        "method": 'POST',
+        "headers": {
+            "Content-Type": "application/json"
+        },
+        "body": data
+    })
+    .then(resp => {
+        return resp.json();
+    })
+    .then(data => {
+        if(data.length > 0) {
+            localStorage.setItem("userdata", JSON.stringify(data[0]));
+            window.location.href = '../home';
+        } else {
+            alert("Usuario ou senha inválidos");
+        }
+    })
+}
+
+function registrar() {
+    let data = JSON.stringify({
+        email: email.value,
+        senha:md5(senha.value),
+        foto: imagem,
+    })
+
+    fetch("http://localhost:3000/usuario", {
+        "method": "POST",
+        "headers": {
+            "Content-Type":"application/json"
+        },
+        "body": data
+    })
+    .then(resp => {
+        return resp.json();
+    })
+    .then(data => {
+        if(data.id !== undefined) {
+            localStorage.setItem("userdata", JSON.stringify(data));
+            window.location.href = '../home';
+        } else {
+            alert("Falha ao cadastrar");
+        }
+    })
+}
+
+function cadastrar() {
+    login.innerHTML = "Cadastrar";
+    login.onclick = registrar;
+    cadastro.hidden = true;
+    camera.hidden = false;
+}
