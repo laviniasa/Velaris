9ae7af94d8759f728d43e49edb18ab9bf2c275c1
MiichaelJose <michaeljosecraft1@gmail.com>
2022-03-15 11:26:08 -0300

    att sql

--- banco_digital/script.sql
+++ banco_digital/script.sql
@@ -1,7 +1,9 @@
 drop database banco;
 create DATABASE banco charset=UTF8 collate utf8_general_ci;
 use banco;
-	CREATE TABLE tb_cliente (
+
+
+	CREATE TABLE tb_clientes (
 	id_cliente integer PRIMARY KEY auto_increment,
 	nome varchar(30) not null,
 	cpf varchar(30) not null,
@@ -13,14 +15,17 @@ use banco;
 	telefone varchar(25)
 	);
 	
-	CREATE TABLE tb_administrador (
+	CREATE TABLE tb_administradores (
+	id_administrador integer PRIMARY KEY auto_increment,
 	nome VARCHAR(30) not null,
 	cpf INTEGER (11) not null,
 	email varchar (50),
 	senha varchar(15) not null
 	);
 	
-	CREATE TABLE tb_cartao (
+	CREATE TABLE tb_cartoes (
+	id_conta integer not null,
+	id_cartao integer not null PRIMARY KEY auto_increment,
 	tipo VARCHAR(30) not null,	
 	numero INTEGER(10) not NULL,
 	validade INTEGER(6) not NULL,
@@ -29,8 +34,9 @@ use banco;
 	limite VARCHAR(30)
 	);
 	
-	CREATE TABLE tb_conta(
-	id_cliente varchar(30) not null,
+	CREATE TABLE tb_contas (
+	id_conta integer not null PRIMARY KEY auto_increment,
+	id_cliente int not null,
 	id_transacao int not null,
 	limite int not null,
 	numero_conta int not null,
@@ -39,34 +45,33 @@ use banco;
 	);
 	
 	CREATE TABLE tb_pix(
+	id_pix int not null primary key auto_increment,
 	chave int not null,
 	cpf_cnpj int not null,
 	email varchar(50) not null,
 	id_cliente varchar(30) not null,
-	id_pix int not null,
 	status VARCHAR(30) not null,
 	telefone VARCHAR(11) not null
 	);
 	
-	CREATE TABLE tb_transacao(
+	CREATE TABLE tb_transacoes (
+	id_cliente integer not null,
+	id_transacao int not null primary key auto_increment,
 	data int(11) not null,
 	tipo varchar(30) not null,
 	valor int not null
+	); 
+
+	CREATE TABLE tb_parcelas(
+	id_parcela integer not null primary key auto_increment,
+	id_cartao int not null,
+	data date not null
 	);
 	
-	CREATE TABLE tb_usuario(
-	data_cadastro int(11) not null,
-	id int not null,
-	nome varchar(100) not null,
-	senha varchar(15) not null,
-	usuario varchar(50) 
-	);
-	
-	select*from tb_cliente;
-	select*from tb_administrador;
-	select*from tb_cartao;
-	select*from tb_conta;
-	select*from tb_pix;
-	select*from tb_transacao;
-	select*from tb_usuario;
+	select * from tb_clientes;
+	select * from tb_administradores;
+	select * from tb_cartoes;
+	select * from tb_contas;
+	select * from tb_pix;
+	select * from tb_transacoes;
 	show tables; 
